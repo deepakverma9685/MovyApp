c128ae176bf4643db718297da7ad9007a377d4be
ACSIND\52263440 <deepakkumar.verma@conduent.com>
2023-10-08 16:55:27 +0530

    test fuke added

---
+++ app/src/test/java/com/example/moviez/ui/adapters/MoviesListAdapterTest.kt
@@ -0,0 +1,38 @@
+package com.example.moviez.ui.adapters
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class MoviesListAdapterTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun getCurrentPos() {
+    }
+
+    @Test
+    fun setCurrentPos() {
+    }
+
+    @Test
+    fun onCreateViewHolder() {
+    }
+
+    @Test
+    fun onBindViewHolder() {
+    }
+
+    @Test
+    fun getItemCount() {
+    }
+}
